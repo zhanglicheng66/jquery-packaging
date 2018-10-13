971b3b3ac3f695f1d7450dec71356f67386c8825
zhanglicheng66 <405328994@qq.com>
2018-10-13 18:43:04 +0800

    修改变量

--- packaging.js
+++ packaging.js
@@ -1,10 +1,10 @@
 function getSiblings(node) {/*API*/
-    var allChildren = item3.parentNode.children
+    var allChildren = node.parentNode.children
     var array = {
         length: 0
     }
     for (let i = 0; i < allChildren.length; i++) {
-        if (allChildren[i] !== item3) {
+        if (allChildren[i] !== node) {
             array[array.length] = allChildren[i]
             array.length += 1
         }
