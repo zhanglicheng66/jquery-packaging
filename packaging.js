6be3731e07a91146e04fa0632db6561a83893dd4
zhanglicheng66 <405328994@qq.com>
2018-10-13 15:33:23 +0800

    Initial commit

---
+++ packaging.js
@@ -0,0 +1,32 @@
+function getSiblings(node) {/*API*/
+    var allChildren = item3.parentNode.children
+    var array = {
+        length: 0
+    }
+    for (let i = 0; i < allChildren.length; i++) {
+        if (allChildren[i] !== item3) {
+            array[array.length] = allChildren[i]
+            array.length += 1
+        }
+    }
+    return array
+}
+console.log(getSiblings(item3))
+
+
+
+
+
+
+function addClass(node,classes) {
+    for(let key in classes){
+        var value = classes[key]
+        if(value){
+            node.classList.add(key)
+        }else{
+            node.classList.remove(key)
+        }
+    }
+}
+
+addClass(item3,{a:true,b:false,c:true})
